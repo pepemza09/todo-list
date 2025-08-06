338d0cb48501c1ee4340af16d9c8531f0de2f5ce
Víctor Costa <victor.costa08@gmail.com>
2025-08-06 15:55:27 +0000

    fix entrypoint and app/todo/urls.py

---
+++ app/todo/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('', views.home, name='home'),
+    # path('add/', views.add_todo, name='add_todo'),
+    # path('delete/<int:id>/', views.delete_todo, name='delete_todo'),
+]
